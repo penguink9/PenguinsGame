cccc4acbf0efb49d2f081c01390bd13085d73a77
penguink9 <giangqt2k4@gmail.com>
2025-06-03 23:10:09 +0700

    Add Enemy-Croc

---
+++ Assets/Scripts/Enemy/EnemyPathing.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveDir;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDir * (moveSpeed * Time.fixedDeltaTime));
+
+        if (moveDir.x > 0.01f)
+        {
+            spriteRenderer.flipX = false;  
+        }
+        else if (moveDir.x < -0.01f)
+        {
+            spriteRenderer.flipX = true;  
+        }
+    }
+
+    public void MoveTo(Vector2 targetPosition)
+    {
+        moveDir = targetPosition.normalized; 
+    }
+}
