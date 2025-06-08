9fca153878bf4454ee1876c7b38901fe6b266744
Thangnvd28 <155721245+Thangnvd28@users.noreply.github.com>
2025-06-08 10:19:39 +0700

    EnemyHealt/KnockBack/DeathVFX

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,6 +10,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private GameObject slashAnimPrefab;
     [SerializeField] private Transform slashAnimSpawnPoint;
+    [SerializeField] private Transform attackCollider;
+
+    public static PlayerMovement Instance;
     private PlayerController playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
@@ -24,6 +27,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Awake()
     {
+        Instance = this;
         playerControls = new PlayerController();
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
@@ -77,6 +81,7 @@ public class PlayerMovement : MonoBehaviour
             rb.linearVelocity = Vector2.zero; 
             AdjustPlayerFacingDirection(); // Cập nhật FacingLeft ngay lập tức
             myAnimator.SetTrigger("Attack");
+            attackCollider.gameObject.SetActive(true);
 
             Vector3 spawnPosition = slashAnimSpawnPoint.position;
 
@@ -119,6 +124,11 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    public void DoneAttackingAnimEvent()
+    {
+        attackCollider.gameObject.SetActive(false);
+    }
+
     private IEnumerator AttackCooldownCoroutine()
     {
         // Đợi cho đến khi cooldown kết thúc
@@ -137,18 +147,21 @@ public class PlayerMovement : MonoBehaviour
         {
             mySpriteRender.flipX = true;
             FacingLeft = true;
+            attackCollider.transform.rotation = Quaternion.Euler(0, -180, 0);
+
         }
         else
         {
             mySpriteRender.flipX = false;
             FacingLeft = false;
+            attackCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
+
         }
     }
 
     private void Start()
     {
 
-      
         playerControls.Combat.Attack.started += _ => Attack();
 
     }
