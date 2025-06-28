3366e7e2a329a8710c00877a83c2ef65845b335b
penguink9 <giangqt2k4@gmail.com>
2025-06-28 15:20:10 +0700

    Update Setting Button

--- Assets/Scripts/UIManagement/ToggleSettingBtn.cs
+++ Assets/Scripts/UIManagement/ToggleSettingBtn.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class ToggleSettingBtn : MonoBehaviour
 {
     [SerializeField] GameObject settingPanel;
+
+    private void Start()
+    {
+        UIInputManager.Instance.OnSettingOpen += Toggle;
+    }
     public void Toggle()
     {
         if (settingPanel != null)
